2e7c9b3d2e5d41da5a10eeaf36f94f4eedbcb966
ruruisu <zaddozgl@gmail.com>
2022-03-05 13:32:32 -0600

    keypress and mouseover

--- events/app.js
+++ events/app.js
@@ -2,6 +2,22 @@ let obj = {
     name: 'Luis',
     age: 20,
     lastName: 'Garcia',
+    keypress: function(){
+        console.log('A key has been pressed')
+    },
+    mouseOver: function (){
+        console.log('The mouse cursor is above')
+    }
 }
-console.log(obj.name);
-console.log(obj['nombre']);
+//console.log(obj.name); //prints 'Luis'
+//console.log(obj['name']); //prints 'Luis'
+
+let myKey = 'name';
+console.log(obj[myKey]); //prints 'Luis'
+// El codigo usa la string 'name' como key 
+myKey = 'keypress';
+console.log(obj[myKey]()); //prints 'A key has been pressed' and 'undefined' right after
+myKey = 'mouseOver';
+console.log(obj[myKey]());//prints 'The mouse cursor is above' and 'undefined' right after
+
+
